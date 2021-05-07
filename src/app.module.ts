fd4f17bc0d8ef212050f9fc9e82b7347edd2a2fa
Leif Lampater <leiflampater@gmail.com>
2021-05-07 21:37:24 +0200

    feat: add signup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 import { DatabaseModule } from './config/database.module';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
+    AuthModule,
     DatabaseModule,
     ConfigModule.forRoot(),
     TasksModule,
---
+++ src/auth/user.repository.ts
@@ -0,0 +1,30 @@
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import { genSalt, hash } from 'bcrypt';
+import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { DUPLICATE_ERROR_CODE } from './error-codes';
+import { User } from './user.entity';
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User>{
+  async signUp({ username, password }: AuthCredentialsDto): Promise<void> {
+    const salt = await genSalt()
+    const user = new User();
+    user.username = username;
+    user.password = await this.hashPassword(password, salt);
+
+    try {
+      await user.save();
+    } catch ({ code }) {
+      if (code === DUPLICATE_ERROR_CODE) {
+        throw new ConflictException('Username already in use.');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
+
+  private hashPassword(password: string, salt: string): Promise<string> {
+    return hash(password, salt);
+  }
+}
